06f95c7522aa32a8bfaf20918d17c540c9b27581
Numenot <qlv6719@gmail.com>
2022-03-08 19:57:54 +0100

    Added home link in navbar and background image in welcome page

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Inject } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-form',
@@ -12,42 +13,33 @@ export class ProfileFormComponent implements OnInit {
   Username = localStorage.getItem('user');
   user: any = {};
 
-  @Input() userData = {
-    Username: this.user.Username,
-    Password: this.user.Password,
-    Email: this.user.Email,
-    Birthday: this.user.Birthday,
-  };
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
   constructor(
     public fetchApiData: UserRegistrationService,
+    private router: Router,
     public dialogRef: MatDialogRef<ProfileFormComponent>,
     public snackBar: MatSnackBar
   ) { }
 
   //get user details on init
   ngOnInit(): void {
-    this.getUser();
-  }
-
-  //get current user details
-  getUser(): void {
-    const user = localStorage.getItem('user');
-    this.fetchApiData.getUser(user).subscribe((resp: any) => {
-      this.user = resp;
-    });
   }
 
   //edit user details and display green snackbar to confirm
   editUser(): void {
     this.fetchApiData.editUser(this.Username, this.userData).subscribe((resp) => {
-      this.dialogRef.close();
-      this.snackBar.open('Your profile was updated successfully!', 'x', {
-        duration: 4000,
+      console.log(resp);
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('Your user details were updated successfully! Please login again', 'x', {
+        duration: 2000,
         panelClass: ['green-snackbar']
       });
-      setTimeout(() => {
-        window.location.reload();
+      this.router.navigate(['welcome']);
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
       });
     });
   }
